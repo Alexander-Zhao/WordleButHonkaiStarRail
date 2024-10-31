ee212d578ce635f6a7e518c0ae06d6a5748358ec
Alexander Zhao <83994091+Alexander-Zhao@users.noreply.github.com>
2024-10-30 21:54:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,127 @@
+let person;
+function Character (name, namePicture, element, elementPicture, path, pathPicture, rarity, rarityPicture) {
+    this.name = name;
+    this.namePicture = namePicture;
+    this.element = element;
+    this.elementPicture = elementPicture;
+    this.path = path;
+    this.pathPicture = pathPicture;
+    this.rarity = rarity;
+    this.rarityPicture = rarityPicture;
+}
+let blackSwan = new Character ("BlackSwan", "https://i.imgur.com/PujJyge.png", "Wind", "https://i.imgur.com/O7DBp8i.png", "Nihility", "https://i.imgur.com/TS5w8uC.png", true, "https://i.imgur.com/iIzVwSN.png");
+let sparkle = new Character ("Sparkle", "https://i.imgur.com/7UcitVj.png", "Quantum", "https://i.imgur.com/nhs4Phe.png", "Harmony", "https://i.imgur.com/oATviZY.png", true, "https://i.imgur.com/iIzVwSN.png");
+let misha = new Character ("Misha", "https://i.imgur.com/A9VVzj5.png", "Ice", "https://i.imgur.com/Hgzqzyt.png", "Destruction", "https://i.imgur.com/M4ao7qm.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let ruanMei = new Character ("Ruan Mei", "https://i.imgur.com/kSC7HGC.png", "Ice", "https://i.imgur.com/Hgzqzyt.png", "Harmony", "https://i.imgur.com/oATviZY.png", true, "https://i.imgur.com/iIzVwSN.png");
+let drRatio = new Character ("Dr. Ratio", "https://i.imgur.com/sIiQIDG.png", "Imaginary", "https://i.imgur.com/XpdMHLZ.png", "Hunt", "https://i.imgur.com/RPSp11K.png", true, "https://i.imgur.com/iIzVwSN.png");
+let xueyi = new Character ("Xueyi", "https://i.imgur.com/oAPQmZN.png", "Quantum", "https://i.imgur.com/nhs4Phe.png", "Destruction", "https://i.imgur.com/M4ao7qm.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let argenti = new Character ("Argenti", "https://i.imgur.com/f4bOKDJ.png", "Physical", "https://i.imgur.com/TKABnLr.png", "Erudition", "https://i.imgur.com/zreQPnJ.png", true, "https://i.imgur.com/iIzVwSN.png");
+let arlan = new Character ("Arlan", "https://i.imgur.com/vv8njxn.png", "Lightning", "https://i.imgur.com/Cdlb5re.png", "Destruction", "https://i.imgur.com/M4ao7qm.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let asta = new Character ("Asta", "https://i.imgur.com/OMkdZm2.png" , "Fire", "https://i.imgur.com/t85L9hi.png", "Harmony", "https://i.imgur.com/oATviZY.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let bailu = new Character ("Bailu", "https://i.imgur.com/4mEBruP.png", "Lightning", "https://i.imgur.com/Cdlb5re.png", "Abundance", "https://i.imgur.com/wADs7Hc.png", true, "https://i.imgur.com/iIzVwSN.png");
+let blade = new Character ("Blade", "https://i.imgur.com/OdsrwcP.png", "Wind", "https://i.imgur.com/O7DBp8i.png", "Destruction", "https://i.imgur.com/M4ao7qm.png", true, "https://i.imgur.com/iIzVwSN.png");
+let bronya = new Character ("Bronya", "https://i.imgur.com/hQfaDur.png", "Wind", "https://i.imgur.com/O7DBp8i.png", "Harmony", "https://i.imgur.com/oATviZY.png", true, "https://i.imgur.com/iIzVwSN.png");
+let clara = new Character ("Clara", "https://i.imgur.com/Bgm5UbI.png", "Physical", "https://i.imgur.com/TKABnLr.png", "Destruction", "https://i.imgur.com/M4ao7qm.png", true, "https://i.imgur.com/iIzVwSN.png");
+let imbibitorLunae = new Character ("Dan Heng Imbibitor Lunae", "https://i.imgur.com/BP5HRHD.png", "Imaginary", "https://i.imgur.com/XpdMHLZ.png", "Destruction", "https://i.imgur.com/M4ao7qm.png", true, "https://i.imgur.com/iIzVwSN.png");
+let danHeng = new Character ("Dan Heng", "https://i.imgur.com/JBOjerq.png", "Wind", "https://i.imgur.com/O7DBp8i.png", "Hunt", "https://i.imgur.com/RPSp11K.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let fuXuan = new Character ("Fu Xuan", "https://i.imgur.com/BdN7aoH.png", "Quantum", "https://i.imgur.com/nhs4Phe.png", "Preservation", "https://i.imgur.com/f6kj7jF.png", true, "https://i.imgur.com/iIzVwSN.png");
+let gepard = new Character ("Gepard", "https://i.imgur.com/BiQsXC3.png", "Ice", "https://i.imgur.com/Hgzqzyt.png", "Preservation", "https://i.imgur.com/f6kj7jF.png", true, "https://i.imgur.com/iIzVwSN.png");
+let guanaifen = new Character ("Guanaifen", "https://i.imgur.com/ay4gKo9.png", "Fire", "https://i.imgur.com/t85L9hi.png", "Nihility", "https://i.imgur.com/TS5w8uC.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let hanya = new Character ("Hanya", "https://i.imgur.com/ysD3xP6.png", "Physical", "https://i.imgur.com/TKABnLr.png", "Harmony", "https://i.imgur.com/oATviZY.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let herta = new Character ("Herta", "https://i.imgur.com/qVLhsj6.png", "Ice", "https://i.imgur.com/Hgzqzyt.png", "Erudition", "https://i.imgur.com/zreQPnJ.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let himeko = new Character ("Himeko", "https://i.imgur.com/OOa77zU.png", "Fire", "https://i.imgur.com/t85L9hi.png", "Erudition", "https://i.imgur.com/zreQPnJ.png", true, "https://i.imgur.com/iIzVwSN.png");
+let hook = new Character ("Hook", "https://i.imgur.com/lXEc87S.png", "Fire", "https://i.imgur.com/t85L9hi.png" , "Destruction", "https://i.imgur.com/M4ao7qm.png" , false, "https://i.imgur.com/hPBzXHQ.png");
+let huohuo  = new Character ("Huo Huo", "https://i.imgur.com/Tp4M9cZ.png", "Wind", "https://i.imgur.com/O7DBp8i.png", "Abundance", "https://i.imgur.com/wADs7Hc.png", true, "https://i.imgur.com/iIzVwSN.png");
+let jingYuan = new Character ("Jing Yuan", "https://i.imgur.com/Unaa6ek.png", "Lightning", "https://i.imgur.com/Cdlb5re.png", "Erudition", "https://i.imgur.com/zreQPnJ.png", true, "https://i.imgur.com/iIzVwSN.png");
+let jingliu = new Character ("Jingliu", "https://i.imgur.com/b0R9RHL.png", "Ice", "https://i.imgur.com/Hgzqzyt.png", "Destruction", "https://i.imgur.com/M4ao7qm.png", true, "https://i.imgur.com/iIzVwSN.png");
+let kafka = new Character ("Kafka", "https://i.imgur.com/9e2U8Ho.png", "Lightning", "https://i.imgur.com/Cdlb5re.png", "Nihility", "https://i.imgur.com/TS5w8uC.png", true, "https://i.imgur.com/iIzVwSN.png");
+let luka = new Character ("Luka", "https://i.imgur.com/EGBmQKv.png", "Physical", "https://i.imgur.com/TKABnLr.png", "Nihility", "https://i.imgur.com/TS5w8uC.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let luocha = new Character ("Luocha", "https://i.imgur.com/pXE2GTv.png", "Imaginary", "https://i.imgur.com/XpdMHLZ.png", "Abundance", "https://i.imgur.com/wADs7Hc.png", true, "https://i.imgur.com/iIzVwSN.png");
+let lynx = new Character ("Lynx", "https://i.imgur.com/HTmjyL7.png", "Quantum", "https://i.imgur.com/nhs4Phe.png", "Abundance", "https://i.imgur.com/wADs7Hc.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let march7th = new Character ("March 7th", "https://i.imgur.com/wJ0AjSR.png", "Ice", "https://i.imgur.com/Hgzqzyt.png", "Preservation", "https://i.imgur.com/f6kj7jF.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let natasha = new Character ("Natasha", "https://i.imgur.com/Khb8cPs.png", "Physical", "https://i.imgur.com/TKABnLr.png", "Abundance", "https://i.imgur.com/wADs7Hc.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let pela = new Character ("Pela", "https://i.imgur.com/3f5gK9A.png", "Ice", "https://i.imgur.com/Hgzqzyt.png", "Nihility", "https://i.imgur.com/TS5w8uC.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let qingque = new Character ("Qingque", "https://i.imgur.com/C12jyol.png", "Quantum", "https://i.imgur.com/nhs4Phe.png", "Erudition", "https://i.imgur.com/zreQPnJ.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let sampo = new Character ("Sampo", "https://i.imgur.com/VMRN9FC.png", "Wind", "https://i.imgur.com/O7DBp8i.png", "Nihility", "https://i.imgur.com/TS5w8uC.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let seele = new Character ("Seele", "https://i.imgur.com/f8NyNfM.png", "Quantum", "https://i.imgur.com/nhs4Phe.png", "Hunt", "https://i.imgur.com/RPSp11K.png", true, "https://i.imgur.com/iIzVwSN.png");
+let serval = new Character ("Serval", "https://i.imgur.com/0FF2r8z.png", "Lightning", "https://i.imgur.com/Cdlb5re.png", "Erudition", "https://i.imgur.com/zreQPnJ.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let silverWolf = new Character ("Silver Wolf", "https://i.imgur.com/QKyLfPi.png", "Quantum", "https://i.imgur.com/nhs4Phe.png", "Nihility", "https://i.imgur.com/TS5w8uC.png", true, "https://i.imgur.com/iIzVwSN.png");
+let sushang = new Character ("Sushang", "https://i.imgur.com/K55X3su.png", "Physical", "https://i.imgur.com/TKABnLr.png", "Hunt", "https://i.imgur.com/RPSp11K.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let tingyun = new Character ("Tingyun", "https://i.imgur.com/IwilUXR.png", "Lightning", "https://i.imgur.com/Cdlb5re.png", "Harmony", "https://i.imgur.com/oATviZY.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let topaz = new Character ("Topaz", "https://i.imgur.com/rrKyKhp.png", "Fire", "https://i.imgur.com/t85L9hi.png", "Hunt", "https://i.imgur.com/RPSp11K.png", true, "https://i.imgur.com/iIzVwSN.png");
+let trailblazerFire = new Character ("Trailblazer (Fire)", "https://i.imgur.com/NHfn51q.png", "Fire", "https://i.imgur.com/t85L9hi.png", "Preservation", "https://i.imgur.com/f6kj7jF.png", true, "https://i.imgur.com/iIzVwSN.png");
+let trailblazerPhysical = new Character ("Trailblazer (Physical)", "https://i.imgur.com/kX1Z82k.png" , "Physical", "https://i.imgur.com/TKABnLr.png", "Destruction", "https://i.imgur.com/M4ao7qm.png", true, "https://i.imgur.com/iIzVwSN.png");
+let welt = new Character ("Welt", "https://i.imgur.com/v7lXpXb.png", "Imaginary", "https://i.imgur.com/XpdMHLZ.png", "Nihility", "https://i.imgur.com/TS5w8uC.png", true, "https://i.imgur.com/iIzVwSN.png");
+let yanqing = new Character ("Yanqing", "https://i.imgur.com/ICl8R8V.png", "Ice", "https://i.imgur.com/Hgzqzyt.png", "Hunt", "https://i.imgur.com/RPSp11K.png", true, "https://i.imgur.com/iIzVwSN.png");
+let yukong = new Character ("Yukong", "https://i.imgur.com/rhbxuib.png", "Imaginary", "https://i.imgur.com/XpdMHLZ.png", "Harmony", "https://i.imgur.com/oATviZY.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let acheron = new Character ("Acheron", "https://i.imgur.com/olbCC4m.png", "Lightning", "https://i.imgur.com/Cdlb5re.png", "Nihility", "https://i.imgur.com/TS5w8uC.png", true, "https://i.imgur.com/iIzVwSN.png");
+let gallagher = new Character ("Gallagher", "https://i.imgur.com/lbFWhqh.png", "Fire", "https://i.imgur.com/t85L9hi.png", "Abundance", "https://i.imgur.com/wADs7Hc.png", false, "https://i.imgur.com/hPBzXHQ.png");
+let aventurine = new Character ("Aventurine", "https://i.imgur.com/RBlrvEY.png", "Imaginary", "https://i.imgur.com/XpdMHLZ.png", "Preservation", "https://i.imgur.com/f6kj7jF.png", true, "https://i.imgur.com/iIzVwSN.png");
+let tries = 1;
+let characters = [blackSwan, sparkle, misha, ruanMei, drRatio, xueyi, argenti, arlan, asta, bailu, blade, bronya, clara, imbibitorLunae, danHeng, fuXuan, gepard, guanaifen, hanya, herta, himeko, hook, huohuo, jingYuan, jingliu, kafka, luka, luocha, lynx, march7th, natasha, pela, qingque, sampo, seele, serval, silverWolf, sushang, tingyun, topaz, trailblazerFire, trailblazerPhysical, welt, yanqing, yukong, acheron, gallagher, aventurine];
+let daily = "Unchosen";
+function dailyRoll() {
+    daily = characters[Math.round(Math.random() * characters.length)];
+};
+dailyRoll();
+function checkAttributes() {
+  if (person.name == daily.name) {
+    document.getElementById("c" + tries + "2").src = "https://i.imgur.com/oS4hhZG.png";
+    document.getElementById("c" + tries + "4").src = "https://i.imgur.com/oS4hhZG.png";
+    document.getElementById("c" + tries + "6").src = "https://i.imgur.com/oS4hhZG.png";
+    document.getElementById("c" + tries + "8").src = "https://i.imgur.com/oS4hhZG.png";
+    win();
+  } else {
+      document.getElementById("c" + tries + "2").src = "https://i.imgur.com/iw98NqQ.png";
+      if (person.element == daily.element) {
+          document.getElementById("c" + tries + "4").src = "https://i.imgur.com/oS4hhZG.png";
+      } else {
+        document.getElementById("c" + tries + "4").src = "https://i.imgur.com/iw98NqQ.png";
+      } if (person.path == daily.path) {
+        document.getElementById("c" + tries + "6").src = "https://i.imgur.com/oS4hhZG.png";
+      } else {
+        document.getElementById("c" + tries + "6").src = "https://i.imgur.com/iw98NqQ.png";
+      } if (person.rarity == daily.rarity) {
+        document.getElementById("c" + tries + "8").src = "https://i.imgur.com/oS4hhZG.png";
+      } else {
+        document.getElementById("c" + tries + "8").src = "https://i.imgur.com/iw98NqQ.png";
+      }
+    tries++;
+    if (tries > 8) {
+      loss();
+    }
+    person = null;
+  }
+};
+function win() {
+  let winMessage = "Congratulations! You have guessed correctely, it was " + daily.name + ". You used " + tries + " attempts to guess.";
+  alert(winMessage);
+}
+function loss() {
+  let lossMessage = "Unfortunately you lost. You used all 8 attempts, the character was... " + daily.name + "!";
+  alert(lossMessage)
+}
+function setChoice(num){
+  person = characters[num];
+  for (let i = 1; i < 8; i++) {
+    if (i == 1) {
+      document.getElementById("c" + tries + i).src = person.namePicture;
+    } else if (i == 3) {
+      document.getElementById("c" + tries + i).src = person.elementPicture;
+    } else if (i == 5) {
+      document.getElementById("c" + tries + i).src = person.pathPicture;
+    } else if (i == 7) {
+      document.getElementById("c" + tries + i).src = person.rarityPicture;
+    }
+  }
+}
+function reset() {
+  for(let i = 1; i < 9; i++) {
+    for(let j = 1; j < 9; j++) {
+      document.getElementById("c" + i + j).src = "https://i.imgur.com/H5rlgPs.png"; 
+    }
+  }
+  person = null;
+  tries = 1;
+  dailyRoll();
+}
